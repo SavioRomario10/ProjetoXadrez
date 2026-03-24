c0af34888f98a92c132f243cf7e831a4ae42e41c
Sávio Romário <savioromario10@gmail.com>
2026-03-24 11:16:37 -0300

    Peaces increments

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
+import chess.pieces.King;
+
 import boardgame.Board;
+import boardgame.Position;
 
 public class ChessMatch {
 
@@ -8,8 +11,8 @@ public class ChessMatch {
 
   public ChessMatch() {
     board = new Board(8, 8);
+    initialSetup();
   }
-
   public ChessPiece[][] getPieces(){
     ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
@@ -19,5 +22,9 @@ public class ChessMatch {
       }
     }
     return mat;
+  }
+
+  private void initialSetup() {
+    board.placePiece(new King(board, Color.BLACK), new Position(1,5));
   }
 }
