f54e708591ba007440e474d9f82682241eb94934
Sávio Romário <savioromario10@gmail.com>
2026-03-25 12:18:57 -0300

    Switching player turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 import chess.ChessPosition;
-
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -52,6 +52,12 @@ public class UI {
       throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
     }
   }
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Current player: " + chessMatch.getCurrentPlayer());
+	}
   public static void printBoard(ChessPiece[][] pieces) {
     for(int i =0; i < pieces.length; i++) {
       System.out.print((8 - i) + " ");
